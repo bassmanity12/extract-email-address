c38cdf2e5d0ec21bdbfd91d10386162660153ec3
Basanta <balami@ttu,ee>
2020-02-25 20:58:23 +0200

    wrote some comments

--- extract.py
+++ extract.py
@@ -1,9 +1,9 @@
 import re
 import csv
 
-with open('hello.txt', newline='') as csvfile:
+with open('hello.txt', newline='') as csvfile: #opening the file with text as a csvfile
     spamreader = csv.reader(csvfile, delimiter='\n')
-    regexforemail = re.compile(r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)')
+    regexforemail = re.compile(r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)') #regex to catch all the emails
     for lines in spamreader:
 
         mo = regexforemail.search(str(lines))
@@ -14,10 +14,8 @@ with open('hello.txt', newline='') as csvfile:
             email = re.sub('\'>', "", email)
 
             f = open("results.txt", "a")
-            f.write(email+"\n")
+            f.write(email+"\n") #appending all the emails
 
         print(str(mo))
-        #
-        #
-        # print(lines)
+    
 
